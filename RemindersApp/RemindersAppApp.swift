92f5d5c06baa7652f0483e9a8453d3af23c951e4
sai-prasad-rekha <saiprasad.rekha@nov.com>
2024-12-18 13:13:57 +0530

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Sai Prasad Rekha on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
